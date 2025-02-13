d0314147a444e6a6012962561214c87246ad031f
Evie <ec22426@qmul.ac.uk>
2025-02-13 18:29:51 +0000

    working user model

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, SiteUser 
 
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     pass
+
+
+'''Register the user model to the admin panel'''
+@admin.register(SiteUser)
+class SiteUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'date_of_birth')
+    search_fields = ('username', 'first_name', 'last_name', 'email')
+    list_filter = ('is_active', 'is_staff')
+    ordering = ('username',)
--- api/models.py
+++ api/models.py
@@ -1,18 +1,35 @@
 from django.db import models
-from django.urls import reverse
+#from django.urls import reverse
+from django.contrib.auth.models import AbstractUser, Group, Permission
 
-# Create your models here.
-class PageView(models.Model):
-    count = models.IntegerField(default=0)
 
-    def __str__(self):
-        return f"Page view count: {self.count}"
     
 class Book(models.Model):
     '''
     Class for the model Book
     ''' 
-
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     #placeholder for image file
+
+class SiteUser(AbstractUser):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(default='2000-01-01')
+    password = models.CharField(max_length=100)
+
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    def as_dict(self):
+        return {  
+            'id': self.id,  
+            'username': self.username,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'email': self.email,
+            'date_of_birth': self.date_of_birth,
+            'password': self.password,
+        }
