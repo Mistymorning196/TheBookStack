c48ede3c6d978e0970e42004550ee6de100e622b
Evie <ec22426@qmul.ac.uk>
2025-02-13 18:08:01 +0000

    Book model added

--- api/models.py
+++ api/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
-
-
 class PageView(models.Model):
     count = models.IntegerField(default=0)
 
     def __str__(self):
         return f"Page view count: {self.count}"
+    
+class Book(models.Model):
+    '''
+    Class for the model Book
+    ''' 
+
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    #placeholder for image file
