fd5974796c5d461d069308b715c357add0f753bb
Evie <eviejbishop@gmail.com>
2025-04-26 22:59:05 +0100

    vercel works

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/', // You can leave this as '/' unless you need a different base for production
+  base:  './',
   build: {
     emptyOutDir: true,
     outDir: 'dist', // This will be the output folder where Vite builds the app (you move files from here later)
